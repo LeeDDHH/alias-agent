0a9108b81f942b6a9dab5168ac8fc188531a3280
DongHeon Lee <niche3600@gmail.com>
2021-05-17 07:29:19 +0900

    デフォルトのaliasデータにidを追加

--- src/data/alias.ts
+++ src/data/alias.ts
@@ -1,10 +1,12 @@
 export default {
   alias: [
     {
+      id: 0,
       name: 'home',
       value: 'open .',
     },
     {
+      id: 1,
       name: 'github',
       value: "open 'https://www.github.com'",
     },
