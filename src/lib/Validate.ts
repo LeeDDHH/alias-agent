60a0b4f609d524330da373a55ef01813b0c9eac8
DongHeon Lee <niche3600@gmail.com>
2021-07-15 15:17:55 +0900

    aliasデータのバリデーション

---
+++ src/lib/Validate.ts
@@ -0,0 +1,45 @@
+import Ajv, { JSONSchemaType } from 'ajv';
+
+const _aliasItemDefSchema = {
+  $id: '/aliasItemDef',
+  definitions: {
+    id: {
+      type: 'number',
+      nullable: false,
+    },
+    name: {
+      type: 'string',
+      nullable: false,
+    },
+    value: {
+      type: 'string',
+      nullable: false,
+    },
+  },
+};
+
+const _aliasItemDataSchema: JSONSchemaType<AliasData> = {
+  type: 'array',
+  minItems: 0,
+  items: {
+    type: 'object',
+    additionalProperties: false,
+    properties: {
+      id: { $ref: '/aliasItemDef#/definitions/id' },
+      name: { $ref: '/aliasItemDef#/definitions/name' },
+      value: { $ref: '/aliasItemDef#/definitions/value' },
+    },
+    required: ['id', 'name', 'value'],
+  },
+};
+
+const validateAliasData = (data: AliasData) => {
+  const ajv = new Ajv();
+
+  const validate = ajv
+    .addSchema(_aliasItemDefSchema)
+    .compile(_aliasItemDataSchema);
+  return validate(data) ? true : false;
+};
+
+export { validateAliasData };
